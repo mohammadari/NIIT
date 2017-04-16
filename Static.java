9cc9b5988882acca9fe8c422d6e13778320c70d8
mohammadari <mohammadari1995@gmail.com>
2017-04-16 11:47:04 -1200

    this is the second commit

---
+++ Static.java
@@ -0,0 +1,16 @@
+
+
+public class Static {
+static int i=90;
+int j=20;
+public static void main(String[]args){
+System.out.println(Static.i);
+Static obj1=new Static();
+System.out.println(obj1.j);
+System.out.println();
+obj1.j=30;
+Static obj2=new Static();
+System.out.println(obj2.j);
+System.out.println(obj1.j);
+}
+}
